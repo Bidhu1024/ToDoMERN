4e6972d199fc71c51a5baa44fe0e09c33205ebbf
Bidhu1024 <bidhu1024av@gmail.com>
2024-12-19 20:56:07 +0530

    connected mongo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose')
-
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        console.log('connected to Mongo')
-    }catch(err){
-        console.log('Database connection failed with error',err)
-        process.exit(1)
-    }
-}
-module.exports = connectDB
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose.set("strictQuery", true);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected to Mongo");
+  } catch (err) {
+    console.log("Database connection failed with error", err);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -12,23 +12,24 @@ app.use(express.json()) // middleware
 const port = 5000;
 
 //connect to mongoDB
-connectDB
+
 
 //api
 
-app.get('/add-sample', async (req, res) => {
-    try {
-        const sampleTodo = new Todo({
-            title: 'Learn MERN Stack',
-        });
+// app.get('/add-sample', async (req, res) => {
+//     try {
+//         const sampleTodo = new Todo({
+//             title: 'Learn MERN Stack',
+//         });
 
-        await sampleTodo.save(); // Save to the database
-        res.status(201).send('Sample TODO created successfully!');
-    } catch (error) {
-        res.status(500).send('Error creating TODO: ' + error.message);
-    }
-});
+//         await sampleTodo.save(); // Save to the database
+//         res.status(201).send('Sample TODO created successfully!');
+//     } catch (error) {
+//         res.status(500).send('Error creating TODO: ' + error.message);
+//     }
+// });
 
 app.listen(port,()=>{
+    connectDB()
     console.log(`Server is running at ${port}`)
 })
